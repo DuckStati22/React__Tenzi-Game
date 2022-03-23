541f7ad4af3154060add8cafdfe6ec0318a28afe
Alex <tsikra.al@yandex.ru>
2022-03-23 11:38:57 +0300

    Change dice style, replace numbers with dots.
    1) Integrate Font Awesome library.
    2) Refactor <Die /> component.
    3) Edit the style.css.

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiceOne } from '@fortawesome/free-solid-svg-icons';
+import { faDiceTwo } from '@fortawesome/free-solid-svg-icons';
+import { faDiceThree } from '@fortawesome/free-solid-svg-icons';
+import { faDiceFour } from '@fortawesome/free-solid-svg-icons';
+import { faDiceFive } from '@fortawesome/free-solid-svg-icons';
+import { faDiceSix } from '@fortawesome/free-solid-svg-icons';
+
 
 function Die(props) {
   const styles = {
-    backgroundColor: props.isHeld ? "#59E391" : "white",
+    color: props.isHeld ? "#59E391" : "",
+  }
+
+  function numToFaDice(number) {
+    switch (number) {
+      case 1:
+        return faDiceOne
+      case 2:
+        return faDiceTwo
+      case 3:
+        return faDiceThree
+      case 4:
+        return faDiceFour
+      case 5:
+        return faDiceFive
+      case 6:
+        return faDiceSix
+    }
   }
 
   return (
-    <div
-      className="die"
-      style={styles}
-      onClick={props.holdDie}
-    >
-      <h2 className="die__number">{props.value}</h2>
-    </div>
+      <FontAwesomeIcon
+        className="die"
+        style={styles}
+        icon={numToFaDice(props.value)}
+        onClick={props.holdDie}
+      />
   )
 }
 
