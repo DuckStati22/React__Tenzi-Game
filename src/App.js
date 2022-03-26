9642ebc806c4ee452c0ddf52aa0b38bf40c1f111
Alex <tsikra.al@yandex.ru>
2022-03-26 19:53:34 +0300

    Add statistics (timer and rolls counter)
    1) Create 'Play Game' button.
    2) Create statistics state and functions to control the state
    3) Add ability to save best score into the localStorage

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import { nanoid } from 'nanoid';
 import Confetti from 'react-confetti';
 
 function App() {
+  const [isStarted, setIsStarted] = React.useState(false);
+  const [statistics, setStatistics] = React.useState(
+    {
+      time: {minutes: 0, seconds: 0},
+      rolls: 1
+    }
+  );
+  const time = statistics.time;
   const [diceArray, setDiceArray] = React.useState(allNewDice());
   const [tenzi, setTenzi] = React.useState(false);
 
-
-  React.useEffect(() => {
-    const allHeld = diceArray.every(die => die.isHeld)
-    const firstValue = diceArray[0].value
-    const allSameValue = diceArray.every(die => die.value === firstValue)
-    if (allHeld && allSameValue) {
-      setTenzi(true)
-    }
-  }, [diceArray])
-
+  function startGame() {
+    setIsStarted(prevIsStarted => !prevIsStarted)
+  }
 
   function generateNewDie() {
     const randomNum = Math.ceil(Math.random() * 6);
@@ -42,9 +43,22 @@ function App() {
             die :
             generateNewDie()
       }))
+
+      setStatistics(prevStatistics => {
+        return {
+          ...prevStatistics,
+          rolls: prevStatistics.rolls + 1,
+        }
+      })
     } else {
       setTenzi(false);
       setDiceArray(allNewDice());
+      setStatistics(
+        {
+          time: {minutes: 0, seconds: 0},
+          rolls: 1
+        }
+      )
     }
   }
 
@@ -55,6 +69,53 @@ function App() {
           die
     }))
   }
+
+  function getBestScore() {
+    return JSON.parse(localStorage.getItem('statistics')) || statistics;
+  }
+  const bestMinutes = getBestScore().time.minutes;
+  const bestSeconds = getBestScore().time.seconds;
+
+  function setBestScore() {
+    const bestScore = getBestScore();
+      localStorage.setItem('statistics', JSON.stringify
+        ({
+          time: time.minutes * 60 + time.seconds < bestMinutes * 60 + bestSeconds ? 
+                time : bestScore.time,
+          rolls: statistics.rolls < bestScore.rolls ?
+                 statistics.rolls : bestScore.rolls
+        }))
+  }
+
+  React.useEffect(() => {
+    const allHeld = diceArray.every(die => die.isHeld)
+    const firstValue = diceArray[0].value
+    const allSameValue = diceArray.every(die => die.value === firstValue)
+    if (allHeld && allSameValue) {
+      setTenzi(true)
+      setBestScore()
+    }
+  }, [diceArray])
+
+  React.useEffect(() => {
+    let timer;
+    if (isStarted && !tenzi) {
+      timer = setInterval(() => {
+        setStatistics(prevStatistics => {
+          const {minutes, seconds} = prevStatistics.time
+          return {
+            ...prevStatistics,
+            time:
+              {
+                minutes: seconds === 59 ? minutes + 1 : minutes,
+                seconds: seconds === 59 ? 0 : seconds + 1,
+              },
+          }
+        })
+      }, 1000);
+    }
+    return () => clearInterval(timer)
+  }, [isStarted, tenzi])
 
   const diceElements = diceArray.map(die => (
     <Die
@@ -72,10 +133,33 @@ function App() {
         <h1 className="description__title">Tenzie</h1>
         <p className="description__para">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       </div>
-      <div className="dice-container">
-        {diceElements}
-      </div>
-      <button className="btn" onClick={rollDice}>{tenzi ? 'Play Again' : 'Roll'}</button>
+      {isStarted ?
+      <>
+        <div className="dice-container">
+          {diceElements}
+        </div> 
+        <button className="btn" onClick={rollDice}>{tenzi ? 'Play Again' : 'Roll'}</button>
+        <div className="statistics">
+          <div className="statistics__time">
+            <h2>
+              Time: {time.minutes < 10 ? '0' + time.minutes : time.minutes}:
+                    {time.seconds < 10 ? '0' + time.seconds : time.seconds}
+            </h2>
+            <h2>
+              (Best: {bestMinutes < 10 ? '0' + bestMinutes : bestMinutes}:
+                     {bestSeconds < 10 ? '0' + bestSeconds : bestSeconds})
+            </h2>
+          </div>
+          <div className="statistics__rerolls">
+            <h2>Rolls: {statistics.rolls}</h2>
+            <h2>
+              (Best: {getBestScore().rolls})
+            </h2>
+          </div>
+        </div>
+      </> :
+      <button className="btn" onClick={startGame}>Play Game</button>
+      }
     </main>
   );
 }
